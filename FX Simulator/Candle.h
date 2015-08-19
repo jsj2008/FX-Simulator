15f451efde938971bd43870785781c318a9e31be
yuu <yuuxeno@users.noreply.github.com>
2015-08-19 21:29:56 +0900

    デフォルトのCandleを生成できるようにした。

--- FX Simulator/Candle.h
+++ FX Simulator/Candle.h
@@ -12,6 +12,16 @@
 @class CandleSource;
 
 @interface Candle : Indicator
+@property (nonatomic) UIColor *downColor;
+@property (nonatomic) UIColor *downLineColor;
+@property (nonatomic) UIColor *upColor;
+@property (nonatomic) UIColor *upLineColor;
+
+/**
+ DBには保存されない。
+*/
++ (instancetype)createDefaultCandle;
+
 - (instancetype)initWithCandleSource:(CandleSource *)source;
 - (void)strokeIndicatorFromForexDataChunk:(ForexDataChunk *)chunk displayForexDataCount:(NSInteger)count displaySize:(CGSize)size;
 @end
