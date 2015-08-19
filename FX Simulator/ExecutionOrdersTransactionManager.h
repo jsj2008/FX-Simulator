5f22e30b421fa9d500cc4cc95290430503a446a9
yuu <yuuxeno@users.noreply.github.com>
2015-08-19 13:45:36 +0900

    Update ExecutionOrdersTransactionManager.

--- FX Simulator/ExecutionOrdersTransactionManager.h
+++ FX Simulator/ExecutionOrdersTransactionManager.h
@@ -13,19 +13,13 @@
 @class ExecutionHistoryManager;
 
 @protocol ExecutionOrdersTransactionTarget <NSObject>
-//-(BOOL)execute:(NSArray*)orders;
 @property (nonatomic, readwrite) BOOL inExecutionOrdersTransaction;
-@property (nonatomic, readwrite) FMDatabase *tradeDB;
 @end
 
 @interface ExecutionOrdersTransactionManager : NSObject
-//+(ExecutionOrdersTransactionManager*)sharedManager;
-//-(void)addTransactionTarget:(id<ExecutionOrdersTransactionTarget>)target;
 -(id)initWithOpenPositionManager:(OpenPositionManager*)openPositionManager executionHistoryManager:(ExecutionHistoryManager*)executionHistoryManager;
 -(BOOL)execute:(NSArray*)orders;
 -(void)start;
 -(void)commit;
 -(void)rollback;
-//@property (nonatomic, readonly) BOOL isStart;
-//@property (nonatomic, readonly) FMDatabase *tradeDatabase;
 @end
