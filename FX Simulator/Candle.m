98c28698f3421b91069f67b743b38196de6e21ed
yuu <yuuxeno@users.noreply.github.com>
2015-08-19 21:29:56 +0900

    デフォルトのCandleを生成できるようにした。

--- FX Simulator/Candle.m
+++ FX Simulator/Candle.m
@@ -14,7 +14,24 @@
 #import "ForexDataChunk.h"
 
 @implementation Candle {
-    CandleSource *_source;
+    CandleSource *_source;    
+}
+
++ (instancetype)createDefaultCandle
+{
+    UIColor *upColor = [UIColor colorWithRed:35.0/255.0 green:172.0/255.0 blue:14.0/255.0 alpha:1.0];
+    UIColor *downColor = [UIColor colorWithRed:199.0/250.0 green:36.0/255.0 blue:58.0/255.0 alpha:1.0];
+    
+    CandleSource *source = [CandleSource new];
+    source.displayOrder = 0;
+    
+    Candle *candle = [[Candle alloc] initWithCandleSource:source];
+    candle.upColor = upColor;
+    candle.upLineColor = upColor;
+    candle.downColor = downColor;
+    candle.downLineColor = downColor;
+    
+    return candle;
 }
 
 - (instancetype)initWithIndicatorSource:(IndicatorSource *)source
@@ -31,12 +48,6 @@
     return self;
 }
 
-/*- (instancetype)initWithDefaultCandle
-{
-    UIColor *upColor = [UIColor colorWithRed:35.0/255.0 green:172.0/255.0 blue:14.0/255.0 alpha:1.0];
-    UIColor *downColor = [UIColor colorWithRed:199.0/250.0 green:36.0/255.0 blue:58.0/255.0 alpha:1.0];
-}*/
-
 - (void)strokeIndicatorFromForexDataChunk:(ForexDataChunk *)chunk displayForexDataCount:(NSInteger)count displaySize:(CGSize)size
 {
     if (chunk == nil) {
@@ -48,6 +59,48 @@
     for (SimpleCandle *candle in candles) {
         [candle stroke];
     }
+}
+
+#pragma mark - getter,setter
+
+- (UIColor *)downColor
+{
+    return _source.downColor;
+}
+
+- (void)setDownColor:(UIColor *)downColor
+{
+    _source.downColor = downColor;
+}
+
+- (UIColor *)downLineColor
+{
+    return _source.downLineColor;
+}
+
+- (void)setDownLineColor:(UIColor *)downLineColor
+{
+    _source.downLineColor = downLineColor;
+}
+
+- (UIColor *)upColor
+{
+    return _source.upColor;
+}
+
+- (void)setUpColor:(UIColor *)upColor
+{
+    _source.upColor = upColor;
+}
+
+- (UIColor *)upLineColor
+{
+    return _source.upLineColor;
+}
+
+- (void)setUpLineColor:(UIColor *)upLineColor
+{
+    _source.upLineColor = upLineColor;
 }
 
 @end
