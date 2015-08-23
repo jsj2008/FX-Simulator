33484d047dfa4161f24e599a210b517c9abe242b
yuu <yuuxeno@users.noreply.github.com>
2015-08-23 21:17:51 +0900

    SaveDataのテストを追加。

--- FX SimulatorTests/SaveDataTests.m
+++ FX SimulatorTests/SaveDataTests.m
@@ -10,9 +10,19 @@
 #import <XCTest/XCTest.h>
 
 #import "SaveData.h"
+#import "SaveDataSource.h"
 #import "CoreDataInMemoryManager.h"
 #import "Chart.h"
+#import "ChartChunk.h"
+#import "Currency.h"
 #import "CurrencyPair.h"
+#import "TimeFrame.h"
+#import "FXSTimeRange.h"
+#import "Setting.h"
+#import "Spread.h"
+#import "PositionSize.h"
+#import "Lot.h"
+#import "Money.h"
 
 @interface SaveDataTests : XCTestCase
 
@@ -41,6 +51,55 @@
 - (void)testExample {
     // This is an example of a functional test case.
     XCTAssert(YES, @"Pass");
+}
+
+- (void)testCreateNewSaveData
+{
+    NSUInteger slotNumber = 1;
+    CurrencyPair *currencyPair = [[CurrencyPair alloc] initWithCurrencyPairString:@"EURUSD"];
+    TimeFrame *timeFrame = [[TimeFrame alloc] initWithMinute:15];
+    
+    SaveData *saveData = [SaveData createNewSaveDataFromSlotNumber:slotNumber currencyPair:currencyPair timeFrame:timeFrame];
+    saveData.startTime = [Setting rangeForCurrencyPair:saveData.currencyPair timeScale:saveData.timeFrame].start;
+    saveData.lastLoadedTime = saveData.startTime;
+    saveData.spread = [[Spread alloc] initWithPips:1 currencyPair:saveData.currencyPair];
+    saveData.accountCurrency = [[Currency alloc] initWithCurrencyType:JPY];
+    saveData.startBalance = [[Money alloc] initWithAmount:1000000 currency:saveData.accountCurrency];
+    saveData.positionSizeOfLot = [[PositionSize alloc] initWithSizeValue:10000];
+    saveData.tradePositionSize = [[PositionSize alloc] initWithSizeValue:10000];
+    saveData.isAutoUpdate = YES;
+    saveData.autoUpdateIntervalSeconds = 1.0;
+    
+    [_coreDataManagerInMemory saveContext];
+    
+    NSManagedObjectContext *context = _coreDataManagerInMemory.managedObjectContext;
+    
+    NSFetchRequest *fetchRequest = [NSFetchRequest new];
+    NSEntityDescription * entityDescription = [NSEntityDescription entityForName:NSStringFromClass([SaveDataSource class]) inManagedObjectContext:context];
+    [fetchRequest setEntity:entityDescription];
+    NSPredicate * predicate = [NSPredicate predicateWithFormat:@"(slotNumber = %d)", slotNumber];
+    [fetchRequest setPredicate:predicate];
+    
+    NSError * error2;
+    NSArray * objects = [context executeFetchRequest:fetchRequest error:&error2];
+    
+    SaveData *newSaveData = [[SaveData alloc] initWithSaveDataSource:objects.firstObject];
+    
+    XCTAssertNotNil(newSaveData, @"Not nil");
+    
+    XCTAssertEqual(slotNumber, newSaveData.slotNumber, @"Equal slotNumber");
+    XCTAssertEqualObjects(currencyPair, newSaveData.currencyPair, @"Equal CurrencyPair");
+    XCTAssertEqualObjects(timeFrame, newSaveData.timeFrame, @"Equal TimeFrame");
+    
+    XCTAssertEqualObjects(currencyPair, newSaveData.mainChart.currencyPair, @"Equal CurrencyPair");
+    XCTAssertEqualObjects(timeFrame, newSaveData.mainChart.timeFrame, @"Equal TimeFrame");
+    XCTAssertEqual(YES, newSaveData.mainChart.isSelected, "isSelected YES");
+    
+    [newSaveData.subChartChunk enumerateCharts:^(Chart *chart) {
+        XCTAssertEqualObjects(currencyPair, chart.currencyPair, @"Equal CurrencyPair");
+        XCTAssertEqualObjects(timeFrame, chart.timeFrame, @"Equal TimeFrame");
+    }];
+    
 }
 
 - (void)testPerformanceExample {
