683cb7670a93ea5cf1214fed641ed2aef6319721
yuu <yuuxeno@users.noreply.github.com>
2015-11-09 00:38:12 +0900

    clean

--- FX Simulator/PositionType.h
+++ FX Simulator/PositionType.h
@@ -7,7 +7,6 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
 
 @interface PositionType : NSObject
 
@@ -16,14 +15,8 @@
 
 - (instancetype)initWithShort;
 - (instancetype)initWithLong;
-- (instancetype)initWithString:(NSString*)typeString;
 - (BOOL)isEqualPositionType:(PositionType*)positionType;
 - (instancetype)reverseType;
 - (NSString *)toDisplayString;
-
-/// DBなどに使う。
-- (NSString *)toTypeString;
-
-- (UIColor *)toColor;
 
 @end
