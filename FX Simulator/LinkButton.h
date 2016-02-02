573c5919eef6b3999345784a4d3b9f9a8a5fd269
yuu <yuuxeno@users.noreply.github.com>
2016-02-02 16:33:30 +0900

    アプリの情報などを表示するAboutTableViewControllerを追加

---
+++ FX Simulator/LinkButton.h
@@ -0,0 +1,13 @@
+//
+//  LinkButton.h
+//  FXSimulator
+//
+//  Created by yuu on 2016/01/27.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LinkButton : UIButton
+@property (nonatomic) NSString *url;
+@end
